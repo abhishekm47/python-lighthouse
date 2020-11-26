70e7da62d009323a527ae687d2a7e5a3690841d3
quantamixsol <quantamixsolutions@gmail.com>
2020-11-26 23:57:49 +0000

    lighthouuse API Basic Integration

--- lighthouse.py
+++ lighthouse.py
@@ -3,12 +3,22 @@
 import requests
 import json
 import sys
-from urllib.parse import urlsplit
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from urllib_parse_urlencode import set_query_parameter
+
+
+API_KEY=os.getenv("API_KEY")
+HTTP_request = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed"
 
 def lighthouse_seo(test_url):
     #sys.stdout = open('output.txt', 'wt', encoding="utf-8")
     # api-endpoint
-    URL = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBkurRgGEs3fI-T4eLZ6xTAvX2VTbiGN7s"
+    URL = set_query_parameter(HTTP_request, "key", API_KEY)
+    print(URL)  
     # defining a params dict for the parameters to be sent to the API
     PARAMS = {
         'url': test_url,
@@ -22,19 +32,3 @@ def lighthouse_seo(test_url):
     #print(pprint.pprint(data["lighthouseResult"]["audits"]))
     return data
 
-def lighthouse_seo_mobile(test_url):
-    #sys.stdout = open('output.txt', 'wt', encoding="utf-8")
-    # api-endpoint
-    URL = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBkurRgGEs3fI-T4eLZ6xTAvX2VTbiGN7s"
-    # defining a params dict for the parameters to be sent to the API
-    PARAMS = {
-        'url': test_url,
-        'category': ["seo", "performance", "accessibility", "best-practices"],
-        'strategy': "mobile"
-    }
-    # sending get request and saving the response as response object
-    r = requests.get(url=URL, params=PARAMS, stream=True)
-    # extracting data in json format
-    data = r.json()
-    #print(pprint.pprint(data["lighthouseResult"]["audits"]))
-    return 
